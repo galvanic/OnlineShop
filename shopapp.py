7326ae0b9e706fef7d276ea11cc57ba32858d13e
Galvanic <justinecourty@yahoo.fr>
2014-04-13 11:25:41 +0100

    Start of hackathon day :)

--- shopapp.py
+++ shopapp.py
@@ -1,10 +1,36 @@
-from bottle import route, run, template, static_file
 
+
+"""
+Seems to be problem with an unclosed socket.
+"""
+
+
+from bottle import route, run, template, static_file
+import csv
 
 
 @route('/')
 @route('/<date:int>')
-def shoplist(date=140303):
-    return template("shoplist", date=date, rows=[[]])
+def shoplist(date=220114):
 
-run(host='localhost', port=8080, debug=True)
+    filepath = "%d.csv" % date
+    with open(filepath, "rU") as file:
+        reader = csv.reader(file)
+
+    rows = [row for row in reader]
+
+    from code import interact
+    interact(local=dict( globals(), **locals() ))
+        
+
+    return template("shoplist", date=date, rows=[["Item 1", "Price"]])
+
+
+
+def main():
+    run(host='localhost', port=8080, debug=True)
+    return
+
+
+if __name__ == '__main__':
+    main()
--- shoplist.tpl
+++ shoplist.tpl
@@ -1,12 +1,13 @@
 % # template to generate a HTML table from a list of tuples (or list of lists, or tuple of tuples or ...)
+
 <h1>Displaying shop {{date}}</h1>
 <p>The shop items are as follows:</p>
 <table>
-%for row in rows:
+% for row in rows:
   <tr>
-  %for col in row:
+  % for col in row:
     <td>{{col}}</td>
-  %end
+  % end
   </tr>
-%end
+% end
 </table>
