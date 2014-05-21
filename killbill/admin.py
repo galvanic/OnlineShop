efc3d7a017af3ef371ef1e1a6bc57be67f91bc10
Galvanic <justinecourty@yahoo.fr>
2014-05-22 00:18:06 +0100

    Add homepage view.

--- killbill/admin.py
+++ killbill/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from killbill.models import UserGroup, Person, Shop, Item, ItemFromShop, ItemFromShopToPerson
 
-# Register your models here.
+
+class PersonInline(admin.TabularInline):
+	model = Person
+	extra = 2
+
+class ShopInline(admin.TabularInline):
+	model = Shop
+	extra = 2
+
+class UserGroupAdmin(admin.ModelAdmin):
+	inlines = [PersonInline, ShopInline]
+	# list_display = ()
+
+
+
+admin.site.register(UserGroup, UserGroupAdmin)
+
+admin.site.register(Item)
+admin.site.register(ItemFromShop)
+admin.site.register(ItemFromShopToPerson)
