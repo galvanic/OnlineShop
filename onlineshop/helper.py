b1c74154b6f5b73ff69ee9cecbaed04e844a9e4f
Galvanic <justinecourty@yahoo.fr>
2015-03-10 22:29:01 +0100

    changes to ask() function to make it more useful specifically for onlineshop application

--- onlineshop/helper.py
+++ onlineshop/helper.py
@@ -1,43 +1,39 @@
 import sys
 
-def ask(question, rangeList=["y","yes","oui"], errorText="Enter another value."):
+
+def ask(question, rangeList=['y','yes','oui'], errorText='Enter another value.'):
     """
-    Asks for and returns user's input. End program with "n".
+    Asks for and returns user's input. End program with ''.
     question:   string
     rangeList:  list of (acceptable) values
     errorText:  string
-    
-    Module with function which asks user for something.
-    
+
     If the user input isn't as expected, the function will loop and ask again.
-    
+
     The user gets the option of terminating the program
     or just continuing the program without being asked again:
     The ask function returns without returning the user input, ending the loop.
-    
+
     However, the loop might continue depending on program architecture,
     or there might be a problem since "None" is returned in that case.
     """
-    endprog = ["n", "no", "non", "end", "terminate", "finish", "0", "endprog"]
-    endloop = ["endloop"]
+    endprog = ['', ' ', 'end', 'terminate', 'finish', '0', 'endprog']
+    endloop = ['endloop']
     if rangeList:
         rangeList = map(str, rangeList)
     while True:
         try:
-            user = input(question+" ")
+            user = input('{} '.format(question))
             if user.lower() in endprog:
-                sys.exit("End of program.")
+                sys.exit('End of program.')
             elif user.lower() in endloop:
                 return
-            # maybe give the option of changing ask when looking at rangeList ?
-            # eg. lower() etc ?
-            # can't remember what i meant by this ^
             elif not rangeList:
                 return user
             elif user.lower() not in rangeList:
                 raise ValueError
         except ValueError:
-            print(errorText + "\n")
+            print(errorText + '\n')
             continue
         break
     return user
