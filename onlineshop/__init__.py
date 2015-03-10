ab0a6f74208db3a34844d1f3d11cf9490126029b
Galvanic <justinecourty@yahoo.fr>
2015-03-10 23:33:08 +0100

    New names for some functions, variables and attributes, and clearer wording in places. Change to the ask helper function to since the loop can be terminated with KeyBoardInterrupt. Catch and safely close connection to db if so in a try/except block. Database helper functions mapped out. More likely that an ORM will be used though in the future.
    Please enter the commit message for your changes. Lines starting

--- onlineshop/__init__.py
+++ onlineshop/__init__.py
@@ -1,2 +1,2 @@
 # for use for any app that imports the onlineshop module
-from onlineshop import parse_receipt, calculate_money_spent_by_each
+from onlineshop import parse_receipt, divide_order_bill
--- onlineshop/helper.py
+++ onlineshop/helper.py
@@ -6,32 +6,19 @@ import sys
 
 def ask(question, rangeList=['y','yes','oui'], errorText='Enter another value.'):
     """
-    Asks for and returns user's input. End program with ''.
+    Asks for and returns user's input.
     question:   string
     rangeList:  list of (acceptable) values
     errorText:  string
 
     If the user input isn't as expected, the function will loop and ask again.
-
-    The user gets the option of terminating the program
-    or just continuing the program without being asked again:
-    The ask function returns without returning the user input, ending the loop.
-
-    However, the loop might continue depending on program architecture,
-    or there might be a problem since "None" is returned in that case.
     """
-    endprog = ['', ' ', 'end', 'terminate', 'finish', '0', 'endprog']
-    endloop = ['endloop']
     if rangeList:
         rangeList = map(str, rangeList)
     while True:
         try:
             user = input('{} '.format(question))
-            if user.lower() in endprog:
-                sys.exit('End of program.')
-            elif user.lower() in endloop:
-                return
-            elif not rangeList:
+            if not rangeList:
                 return user
             elif user.lower() not in rangeList:
                 raise ValueError
