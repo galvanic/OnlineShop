5c7ee8ff758e3e569e988d3397f633620dbd95f0
Galvanic <justinecourty@yahoo.fr>
2015-03-18 11:32:21 +0000

    More controllers and views. Webapp version now works minimally.

--- shopapp/routes.py
+++ shopapp/routes.py
@@ -6,53 +6,7 @@ from flask import render_template,\
                   redirect,\
                   url_for
 
-from onlineshop import get_orders,\
-                       get_flatmate_names
-
-from collections import namedtuple
-Order = namedtuple('Order', 'id, delivery_date')
-
-
-###
-### Database stuff
-###
-
-
-import sqlite3
-from flask import g
-
-DB_FILE = '/Users/jc5809/Dropbox/Programming/Projects/OnlineShop/data/onlineshop.db'
-
-def connect_to_database():
-    return sqlite3.connect(DB_FILE)
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_to_database()
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
-###
-### controllers: other
-###
-
-
-@app.route('/style.css')
-def css():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'css/style.css')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
+import onlineshop as api
 
 ###
 ### controllers: main pages
@@ -68,32 +22,49 @@ def index():
     - button to form page to add flatmate(s)
     - button to form page to add order
     """
-    orders = [Order(o_id, delivery_date) for o_id, delivery_date in get_orders(get_db())]
-
     return render_template('index.html',
-        flatmates = get_flatmate_names(get_db()),
-        orders    = orders
+        flatmates = api.get_flatmate_names(),
+        orders    = api.get_orders()
     )
 
 
-@app.route('/order/<int:order_id>/', methods=['POST'])
+@app.route('/order/<int:order_id>/')
 def display_order(order_id):
     """Renders a page with:
     - list of flatmates and their individual contribution to the bill
     - list of purchases and the assigned flatmates next to each purchase
-    - button to form page to re-assign
+    - link to form page to re-assign
     """
+    if api.is_order_assigned:
+        baskets = api.get_order_baskets(order_id)
+
+    purchases = api.get_order_purchases(order_id)
+    purchases = [(p, api.get_purchasers(p['id'])) for p in purchases]
 
     return render_template('display_order.html',
-        purchases = purchases
+        order     = api.get_order(order_id),
+        purchases = purchases,
+        baskets   = None
     )
 
 
 @app.route('/new/flatmate')
-def add_flatmates():
+def new_flatmates():
     """Renders a form page to add flatmates.
     """
-    return render_template('add_flatmates.html')
+    existing_flatmates = api.get_flatmate_names()
+    return render_template('add_flatmates.html',
+        flatmates = existing_flatmates
+    )
+
+
+@app.route('/new/flatmate/add', methods=['POST'])
+def add_flatmates():
+    """"""
+    names = request.form['flatmates'].split()
+    for name in names:
+        api.add_new_flatmate(name)
+    return redirect(url_for('index'))
 
 
 @app.route('/new/order')
@@ -101,7 +72,7 @@ def paste_receipt(default=True):
     """Renders a form page to paste the receipt text into.
     """
     if default:
-        with open('../tests/dummy_receipt.txt', 'rU') as ifile:
+        with open('tests/dummy_receipt.txt', 'rU') as ifile:
             default_shop = ifile.read()
     else:
         default_shop = ''
@@ -110,45 +81,61 @@ def paste_receipt(default=True):
         default_shop = default_shop
     )
 
-
-@app.route('/order/<int:order_id>/parse', methods=['POST'])
-def parse_receipt(order_id):
+@app.route('/new/order/parse', methods=['POST'])
+def parse_receipt():
     """Parses the receipt and redirects to the appropriate next step.
     """
-    # parse
-    receipt_text = request.forms['receipt']
+    receipt_text = request.form['receipt']
+    order_id = api.process_input_order(receipt_text)
 
-    # check if order exists already using the delivery date
-    # if so, check if already assigned
-    # if exists and all purchases are assigned, redirect to the order page
-    return redirect(url_for('display_order',
-        order_id = order_id
-    ))
-
-    # if not, generate a new order id and redirect to the assignment page
-    return redirect(url_for('assign_order',
-        order_id = generate_new_order_id()
-    ))
+    assigned = api.is_order_assigned(order_id)
+    if assigned:
+        return redirect(url_for('display_order',
+            order_id = order_id
+        ))
+    else:
+        return redirect(url_for('assign_order',
+            order_id = order_id
+        ))
 
 
-@app.route('/order/<int:order_id>/assign', methods=['POST'])
+@app.route('/order/<int:order_id>/assign')
 def assign_order(order_id):
     """Renders a form page with a list of purchases and flatmate names 
     next to each purchase to click and assign.
     """
-
     return render_template('assign_order.html',
-        purchases = purchases,
-        flatmates = flatmates
+        order_id  = order_id,
+        purchases = api.get_order_purchases(order_id),
+        flatmates = api.get_flatmate_names()
     )
 
 
-@app.route('/order/<int:order_id>/calculate', methods=['POST'])
-def divide_bill(order_id):
-    """Calculates each flatmate's individual contribution to the bill
-    and redirects to the order page to summarise.
-    """
+@app.route('/order/<int:order_id>/assigning', methods=['POST'])
+def add_basket_items(order_id):
+    """"""
+    purchase_ids = [p['id'] for p in api.get_order_purchases(order_id)]
+    for p_id in purchase_ids:
+        purchasers = request.form.getlist(str(p_id))
+        for name in purchasers:
+            f_id = api.get_flatmate_id(name)
+            api.add_new_basket_item(p_id, f_id)
 
     return redirect(url_for('display_order',
         order_id = order_id
     ))
+
+
+###
+### controllers: other
+###
+
+
+@app.route('/style.css')
+def css():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'css/style.css')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
